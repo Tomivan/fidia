0d1d0201a9a8b5d3add91abcdaacda96560b3632
Tomivan <gracejunkie20@gmail.com>
2021-08-22 21:47:36 +0100

    project setup and login added

---
+++ src/components/dashboard.vue
@@ -0,0 +1,218 @@
+<template>
+    <div>
+        <nav>
+            <h4>Store name</h4>
+            <div class="nav-element">Home</div>
+            <div class="nav-element">
+               <p> Payments</p>
+               <div>
+                   <p>Reviews</p>
+                   <p>Disputes</p>
+                   <p>Top-ups</p>
+                   <p>Collected fees</p>
+                   <p>Transfers</p>
+                   <p>Payouts</p>
+                   <p>All Transactions</p>
+               </div>
+            </div>
+            <div class="nav-element">Balances</div>
+            <div class="nav-element">Customers</div>
+            <div class="nav-element">Connected accounts</div>
+            <div class="nav-element">Products</div>
+            <div class="nav-element">Reports</div>
+            <div class="nav-element">Developers</div>
+            <div class="nav-element">View data test</div>
+            <div class="nav-element">Home</div>
+        </nav>
+        <main class="main">
+            <div class="input">
+                <input type="text" placeholder="Search">
+            </div>
+            <section class="section">
+                <div class="section-top">
+                    <h2>Payments</h2>
+                    <div class="buttons">
+                        <button>Filter</button>
+                        <button>Export</button>
+                        <button>Create Payment</button>
+                    </div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Amount</th>
+                                <th></th>
+                                <th>Description</th>
+                                <th>Customer</th>
+                                <th>Date</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td>US$352.22</td>
+                                <td><span>succeeded</span></td>
+                                <td>fe3d8216-58f9-4dc1-aaef-2f19463b5258</td>
+                                <td>hefovo87@ereyemind...</td>
+                                <td>7 Jul, 14:04</td>
+                                <td></td>
+                            </tr>
+                             <tr>
+                                <td></td>
+                                <td>US$39.99</td>
+                                <td><span>succeeded</span></td>
+                                <td>0d7b6ac7-dbda-4157-bae9-e64749e538f5</td>
+                                <td>john.doe@gmail.com</td>
+                                <td>7 Jul, 14:01</td>
+                                <td></td>
+                            </tr>
+                             <tr>
+                                <td></td>
+                                <td>US$19.99</td>
+                                <td><span>succeeded</span></td>
+                                <td>eca84b25-0324-4f31-b9fb-d489048b77ae</td>
+                                <td>jfamosinhoaraca@iseo...</td>
+                                <td>7 Jul,13:28</td>
+                                <td></td>
+                            </tr>
+                             <tr>
+                                <td></td>
+                                <td>US$19.99</td>
+                                <td><span>succeeded</span></td>
+                                <td>9ce0027a-2429-40aa-bca3-eb9fae9829db</td>
+                                <td>gdontdisable@typery...</td>
+                                <td>7 Jul,13:02</td>
+                                <td></td>
+                            </tr>
+                             <tr>
+                                <td></td>
+                                <td>US$142.17</td>
+                                <td><span>succeeded</span></td>
+                                <td>73bb0713-d326-4983-a805-9cef0f15ff3c</td>
+                                <td>psir@typre.com</td>
+                                <td>7 Jul,12:34</td>
+                                <td></td>
+                            </tr>
+                             <tr>
+                                <td></td>
+                                <td>US$93.76</td>
+                                <td><span>succeeded</span></td>
+                                <td>dff08e3b-037e-4fc9-a587-92850d82868e</td>
+                                <td>qomarkhalildubai0@g...</td>
+                                <td>7 Jul, 12:16</td>
+                                <td></td>
+                            </tr>
+                             <tr>
+                                <td></td>
+                                <td>US$931.00</td>
+                                <td><span>succeeded</span></td>
+                                <td>3005be4a-6d4d-487c-85c3-d61cfe64ef63</td>
+                                <td>kfouzan.saleemc@yah...</td>
+                                <td>7 Jul,11:31</td>
+                                <td></td>
+                            </tr>
+                             <tr>
+                                <td></td>
+                                <td>US$19.99</td>
+                                <td><span>succeeded</span></td>
+                                <td>5a9aae47-1d6a-484e-8305-c75107796cd1</td>
+                                <td>7lucyveenstra918@g...</td>
+                                <td>7 Jul,11:18</td>
+                                <td></td>
+                            </tr>
+                             <tr>
+                                <td></td>
+                                <td>$41.99</td>
+                                <td><span>succeeded</span></td>
+                                <td>48eb8b41-9ff0-4d5d-82cc-4b9b820b5f53</td>
+                                <td>0sajad@yopmail.com</td>
+                                <td>7 Jul,10:46</td>
+                                <td></td>
+                            </tr>
+                             <tr>
+                                <td></td>
+                                <td>US$179.98</td>
+                                <td><span>succeeded</span></td>
+                                <td>5a9aae47-1d6a-484e-8305-c75107796cd1</td>
+                                <td>7lucyveenstra918@g...</td>
+                                <td>7 Jul, 10:12</td>
+                                <td></td>
+                            </tr>
+                             <tr>
+                                <td></td>
+                                <td>US$15.17</td>
+                                <td><span>succeeded</span></td>
+                                <td>48eb8b41-9ff0-4d5d-82cc-4b9b820b5f53</td>
+                                <td>0sajad@yopmail.com</td>
+                                <td>7 Jul,06:03</td>
+                                <td></td>
+                            </tr>
+                             <tr>
+                                <td></td>
+                                <td>US$41.99</td>
+                                <td><span>succeeded</span></td>
+                                <td>02fd1b96-0bba-48aa-8557-81021755b196</td>
+                                <td>feverton78@gmail.com</td>
+                                <td>6 Jul, 23:45</td>
+                                <td></td>
+                            </tr>
+                             <tr>
+                                <td></td>
+                                <td>US$67.83</td>
+                                <td><span>succeeded</span></td>
+                                <td>986bdfca-6c8d-4f40-b2ae-ed0abacccfce</td>
+                                <td>lsid.ali_blidi@outlook...</td>
+                                <td>6 Jul,23:32</td>
+                                <td></td>
+                            </tr>
+                             <tr>
+                                <td></td>
+                                <td>US$35.01</td>
+                                <td><span>succeeded</span></td>
+                                <td>twendel.b@yahoo.com</td>
+                                <td>c5629140-ea34-40d8-97e4-d2986b33275e</td>
+                                <td>6 Jul,23:30</td>
+                                <td></td>
+                            </tr>
+                             <tr>
+                                <td></td>
+                                <td>US$374.93</td>
+                                <td><span>succeeded</span></td>
+                                <td>c2194818-1485-437c-a389-6164e605d697</td>
+                                <td>pdevonneq@gmail.com</td>
+                                <td>6 Jul,23:16</td>
+                                <td></td>
+                            </tr>
+                             <tr>
+                                <td></td>
+                                <td>US$330.19</td>
+                                <td><span>succeeded</span></td>
+                                <td>6583691d-4d9f-43ce-8f2f-0d2d95b30ad4</td>
+                                <td>atotspop886@gmx.com</td>
+                                <td>6 Jul,23:06</td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+            <div class=footer>
+                <p>16 results</p>
+                <div class="page">
+                    <button>Previous<button>
+                    <button>Next</button>
+                </div>
+            </div>
+        </main>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  props: {
+    msg: String
+  }
+}
+</script>
---
+++ src/components/login.vue
@@ -0,0 +1,102 @@
+<template>
+ <div class="login">
+   <h3>Stripe</h3>
+   <form class="form">
+     <h3>Sign in to your account</h3>
+     <label>Email</label>
+     <input type="text" placeholder="jane.doe@gmail.com" />
+     <div class="password">
+       <label>Password</label>
+       <router-link to="#">Forgot your password?</router-link>
+     </div>
+     <input type="password" />
+     <div class="stay-tuned">
+       <input type="checkbox" class="checkbox"><p>Stay signed in for a week</p>
+     </div> 
+     <button class="continue">Continue</button>
+     <p class="sso">Use single sign-on(SSO) instead</p>
+   </form>
+   <p>Don't have an account? <span>Sign up</span></p>
+   <footer>
+     <ul>
+       <li>&#169; Stripe</li>
+       <li>Contact</li>
+       <li>Privacy & Terms</li>
+     </ul>
+   </footer>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped>
+.login{
+  background: F7FAFC;
+  margin: 3% 0 0 30%;
+}
+.form{
+  background:#fff;
+  border-radius: 5px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 50px;
+  width: 30vw;
+  padding: 2em;
+}
+
+input{
+  width: 90%;
+  height: 4vh;
+  margin-top: 2%;
+  border: 1px solid #d9dce1;
+  border-radius: 5px;
+}
+input::placeholder{
+  padding-left: 1em;
+}
+.password{
+  margin-top: 5%;
+}
+router-link{
+  margin-left: 40%;
+  color: #635cff;
+}
+.stay-tuned{
+  margin-top: 5%;
+  display: flex;
+}
+.checkbox{
+  height: 2vh;
+  width: 3%;
+  margin-top: 4.5%;
+}
+.continue{
+  background: #635cff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  width: 92%;
+  height: 6vh;
+}
+.sso{
+  text-align: center;
+  color: #635cff;
+  margin-top: 5%;
+}
+span{
+  color:#635cff;
+}
+ul{
+  display: flex;
+  margin-left: -5%;
+}
+li{
+  margin-left: 3%;
+}
+</style>
